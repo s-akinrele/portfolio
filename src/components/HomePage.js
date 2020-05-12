e06ce41c269aadf7fd2bed7892c9f4fa21a16bd2
Oluwasinmisola Akinrele <akinrelesimisola@yahoo.com>
2020-05-11 22:46:08 -0400

    page hero section

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import PageHero from './PageHero'
+import '../styles/homePage.scss'
 
 const HomePage = () => (
   <div className='home-page'>
     <Banner/>
+    <PageHero />
   </div>
 )
 
